bb338e622d22a2cc49fc688700df8939206a7788
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-05-31 22:20:14 +0400

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
-uv venv --python='3.12'
+uv venv --python='3.13'
 uv pip install --no-cache-dir marimo
 uv pip install --no-cache-dir -r requirements.txt
