31ba35feab21105d7c1bbaedb4e9f280cdf0ef32
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2026-02-24 13:08:46 +0400

    fmt

--- tests/test_monkeys/conftest.py
+++ tests/test_monkeys/conftest.py
@@ -4,6 +4,7 @@ Security Notes:
 - S101 (assert usage): Asserts are appropriate in test code for validating conditions
 - S603/S607 (subprocess usage): Any subprocess calls use controlled inputs in test environments
 """
+
 import numpy as np
 import pytest
 
